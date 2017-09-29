648cd11254fd063440de02c9880124ea9c1d8418
MonteShaffer <monte.shaffer@gmail.com>
2017-09-29 08:23:28 -0400

    dampen

--- mPowerEI/R/submitFeatures.R
+++ mPowerEI/R/submitFeatures.R
@@ -129,6 +129,16 @@ loadSubmitLibraries = function()
   library(kernlab);
   library(nnet);
   library(data.table);
+  
+  # https://cran.r-project.org/web/packages/caretEnsemble/vignettes/caretEnsemble-intro.html
+  library("mlbench");
+  library("rpart");
+  
+  library("caTools");
+  library("gbm");
+  
+  
+  
 }
 
 
@@ -267,5 +277,261 @@ fit_model<-function(training, featurenames, covs_num, covs_fac){
     ))
   return(greedy_ensemble)
 }
+
+
+dampenOutliers = function(dframe,xfeats,iCut=6)
+{
+  # iCut is number of IQR to truncate to
+  pmed = plyr::colwise(median)(dframe[,xfeats]);  
+  pmean = plyr::colwise(mean)(dframe[,xfeats]);
+  psd = plyr::colwise(sd)(dframe[,xfeats]); 
+  pmin = plyr::colwise(min)(dframe[,xfeats]); 
+  pmax = plyr::colwise(max)(dframe[,xfeats]); 
+  piqr = plyr::colwise(IQR)(dframe[,xfeats]);  
+  
+  pcutMin = pmed - iCut*piqr;
+  pcutMax = pmed + iCut*piqr;
+ 
+  rframe = dframe;
+  xlen = length(xfeats);
+  xnames = names(pfeats[,xfeats]);
+  for(i in 1:xlen)
+    {
+    xname = xnames[i];
+      xval = as.numeric(pcutMin[xname]);
+    xidx = xfeats[i];
+    
+    rframe[[xname]][ rframe[[xname]] < xval  ] = xval; 
+      xval = as.numeric(pcutMax[xname]);
+    rframe[[xname]][ rframe[[xname]] > xval  ] = xval; 
+    
+    
+    #rframe[,xidx] = ifelse(x < xval, xval, x);
+    #rframe[,xidx] = ifelse(x > xval, xval, x);
+    }
+  
+  
+  rmed = plyr::colwise(median)(rframe[,xfeats]);  
+  rmin = plyr::colwise(min)(rframe[,xfeats]); 
+  rmax = plyr::colwise(max)(rframe[,xfeats]); 
+  
+rframe;  
+}
+
+
+#' Internal Test of Greedy Ensemble with Predictions and Accuracy
+#'
+#' @param submitme the dataframe with submission, not entire synapseSubmission, $r and $h are required.
+#' @param split number, 1-99, what percent of the data will be sampled into train vs test set (e.g., 80 means 80 into train, 20 into 'out of sample' test)
+#' @param dv string, the name of the column that will be the dv
+#' @param dvv numeric vector, the values of the DV that will be classified, first = 0, second = 5
+#' @param dvf string vector, the factors of the DV that will be classified, first = 'PD.off', second = 'nonPD'
+#' @param xfeats numeric vector, the column numbers that define the features
+#' @param dreplace string, how do we replace duplicates in analysis set, "first" is just the first value, "median" is the median of all values, "none" skips this step...
+#'
+#' @return list of items from caret, model = , ensemble = , predict = , accuracy = , control = 
+#' @export
+#'
+
+mPower_test<-function(submitme, dv="healthState", dvv = c(0,5), dvf=c("PD.off","nonPD"), xfeats=1:20, split=80, dreplace="none")
+  {
+  timerx = NULL; timeru = NULL; timeri = NULL;
+  
+  tstart = Sys.time();
+  # submitme has some noise applied to imputate, so if we re-imputate, we will get slightly different values ...
+  # this function samples based on 'split' so a given run will have slightly different values
+  # $r and $h are for recordId and healthCode
+  
+  play = submitme;
+  
+  # subset based on dvv
+  psets = NULL;
+  for(dva in dvv)
+    {
+    print(dva);
+    pset = subset(play, play[[dv]] == dva);
+    psets = rbind(psets,pset);
+    }
+  
+  play = psets;
+  #set.seed(9876); # if you want repeatable
+  play = play[sample(nrow(play)),];  # sample by row ... randomize
+  
+  myC = c();
+    myC = c(myC, which(colnames(play)=="h"));
+    myC = c(myC, which(colnames(play)=="r"));
+    myC = c(myC, which(colnames(play)==dv));
+    myC = c(myC, xfeats);
+  
+  xlen = length(xfeats);
+  xpos = 4:(3+xlen);
+  
+  playN = dim(play)[1];
+  playS = floor(split/100 * playN);
+  
+  ptrain = play[1:playS,myC];           # 14340
+  ptest  = play[(1+playS):playN,myC];   #  3585  [0.25]
+  
+  tend = Sys.time(); timer = tend - tstart; print(timer); flush.console();
+    timerx = c(timerx,as.character(timer));
+    timeru = c(timeru, attr(timer,"units") );
+    timeri = c(timeri, "init");
+  
+  
+  
+  tstart = Sys.time();
+  if(dreplace != "none")
+  {
+  # replace duplicates with "median" or merely "first value"
+    ptrain = replaceDuplicates(ptrain,xpos,dreplace); # 2354
+    ptest = replaceDuplicates(ptest,xpos,dreplace);   # 1218   [0.52]
+  }
+  tend = Sys.time(); timer = tend - tstart; print(timer); flush.console();
+ 
+    timerx = c(timerx,as.character(timer));
+    timeru = c(timeru, attr(timer,"units") );
+    timeri = c(timeri, "duplicates");
+  
+  
+  tstart = Sys.time();
+  ## fit model
+  model.dv = ptrain[,3];
+    model.dvf = factor(model.dv,levels=dvv,labels=dvf);
+    model.features = ptrain[,xpos];
+    model.features.names = names(model.features);
+  
+  
+  print("Run Control"); flush.console(); 
+  set.seed(1234)
+  model.control <- trainControl(  method="boot",
+                              number = 50,
+                              savePredictions="final",
+                              classProbs=TRUE,
+                              index=createResample(model.dvf, 50),
+                              summaryFunction=twoClassSummary );
+  
+  tmpM = model.matrix( ~ ., model.features);  # useful if we have factors - factors to dummies
+                                              # we are not allowed to include factors, so?
+                                              # [,-1] removes the intercept
+  
+  print(dim(model.features));
+  print(length(model.dv));
+  print(table(model.dvf));
+  
+  tend = Sys.time(); timer = tend - tstart; print(timer);
+  
+  timerx = c(timerx,as.character(timer));
+  timeru = c(timeru, attr(timer,"units") );
+  timeri = c(timeri, "control");
+  
+  
+  
+  
+  tstart = Sys.time();
+  print("Caret Call"); flush.console();
+  model.list <- caretList(
+    y=model.dvf , x=model.features,
+    trControl=model.control,
+    metric = "ROC",
+    tuneList=list(glmnet = caretModelSpec(method = "glmnet"), 
+                  rf = caretModelSpec(method="rf"), # random forest
+                  svmLinear = caretModelSpec(method="svmLinear"), 
+                  knn = caretModelSpec(method="knn"), 
+                  nnet = caretModelSpec(method = "nnet", trace=F)),
+    continue_on_fail = FALSE, preProc = c("center", "scale")
+  )
+  
+  
+  tend = Sys.time(); timer = tend - tstart; print(timer);
+  
+  timerx = c(timerx,as.character(timer));
+  timeru = c(timeru, attr(timer,"units") );
+  timeri = c(timeri, "caret");
+  
+  
+  
+  tstart = Sys.time();
+  
+  print("Ensemble run"); flush.console();
+  greedy_ensemble <- caretEnsemble(
+    model.list, 
+    metric="ROC",
+    trControl=trainControl(
+      number=length(model.list),
+      summaryFunction=twoClassSummary,
+      classProbs=TRUE
+    ))
+  
+  
+  
+  
+  tend = Sys.time(); timer = tend - tstart; print(timer);
+  
+  timerx = c(timerx,as.character(timer));
+  timeru = c(timeru, attr(timer,"units") );
+  timeri = c(timeri, "ensemble");
+  
+  
+  
+  
+  
+  }
+
+
+
+
+#' Replace 'healthCode' duplicates in dataframe
+#'
+#' @param dframe dataframe
+#' @param xpos positions of xfeats that need to be analyzed
+#' @param dreplace string, how to replace with "first" value or "median"
+#'
+#' @return updated dataframe without replicates
+#' @export
+#'
+
+replaceDuplicates = function(dframe, xpos, dreplace="first")
+{
+  
+  rframe = NULL;
+  myHs = NULL;
+  # stack approach, we are not going to adjust order .. not efficient
+  dlen = dim(dframe)[1];
+  
+  for(i in 1:dlen)
+  {
+  #print(paste(i," of ",dlen)); flush.console();  
+    drow = dframe[i,];
+    dh = as.character(drow$h);
+  #print(dh);
+    # index 3 is the dv
+    dv = as.numeric(drow[3]);
+    
+    if( length( which(myHs == dh) ) < 1 )
+    {
+      # not found
+      if(dreplace=="first") {
+        dreplacerow = drow;
+      } else if(dreplace=="median") {
+        dsub = subset(dframe,dframe$h==dh);
+          dmed = plyr::colwise(median)(dsub[,c(3,xpos)]);  # median of state?
+        dreplacerow = drow;
+        dreplacerow[xpos] = dmed[-1];
+      }
+      
+    
+    rframe = rbind(rframe,dreplacerow);    
+    myHs = c(myHs,dh); # added to stack
+    }
+      
+    
+  }
+  
+  
+  
+  rframe;
+  
+}
+
 
 
