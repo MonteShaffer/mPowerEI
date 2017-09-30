b9fdaf81f57a710fda9ef766905a109cd63178d3
MonteShaffer <monte.shaffer@gmail.com>
2017-09-30 11:22:47 -0400

    motion features

--- mPowerEI/R/buildFeatures.R
+++ mPowerEI/R/buildFeatures.R
@@ -42,6 +42,99 @@ getMotionObject = function (rv)
 }
 
 
+
+
+#' Motion Features
+#'
+#' @param rvObj 
+#'
+#' @return list of features
+#' @export
+#'
+
+getMotionFeaturesFromRecord = function(rvObj)
+{
+  
+  # rvObj = getMotionObject(rv);
+  plist = pinfo = list();
+  
+  stop("todo: getmotionfeatures");
+  
+  for(act in setup$acts)
+  {
+    pinfo[[act]] = rvObj$raw$tlist[[act]]$pedometer;
+    plen = length(pinfo[[act]]);
+    if(plen > 2)
+    {
+      # let's not record if less than 2 data points, I don't care if rest [unlikely] or outbound/return ... I will merge into a final list ...
+      
+      # plist = rbind(plist,pinfo[[act]][,5:16]);
+      plist = rbind(plist,pinfo[[act]][,5:13]);
+    }
+    
+  }
+  
+  
+  plen = dim(plist)[1];
+  
+  
+  medianDDPS = medianDSPS = medianDD = medianDS = medianDPS = medianSPS = NA;
+  iqrDDPS = iqrDSPS = iqrDD = iqrDS = iqrDPS = iqrSPS = NA;  
+  
+  mindeviationDPS = maxdeviationDPS = mindeviationDDPS = maxdeviationDDPS = NA;
+  mindeviationSPS = maxdeviationSPS = mindeviationDSPS = maxdeviationDSPS = NA;
+  
+  if(!is.null(plen))
+  {
+    # medians ... biased based on height
+    med = plyr::colwise(median)(plist);
+    medianDDPS = med$deltaDistancePerSecond;
+    medianDSPS = med$deltaStepsPerSecond;
+    medianDD = med$deltaDistance;
+    medianDS = med$deltaSteps;
+    medianDPS = med$distancePerSecond;
+    medianSPS = med$stepsPerSecond;
+    
+    # IQR ... biased based on height
+    iqrDDPS = IQR(plist$deltaDistancePerSecond);
+    iqrDSPS = IQR(plist$deltaStepsPerSecond);
+    iqrDD = IQR(plist$deltaDistance);
+    iqrDS = IQR(plist$deltaSteps);
+    iqrDPS = IQR(plist$distancePerSecond);
+    iqrSPS = IQR(plist$stepsPerSecond);
+    
+    # normMin / normMax   ... standardize to min/max values ... independent of height
+    
+    cinfo = computeNormValue(plist$deltaDistancePerSecond);
+    mindeviationDDPS = cinfo$ddeviationmin;
+    maxdeviationDDPS = cinfo$ddeviationmax;
+    
+    cinfo = computeNormValue(plist$distancePerSecond);
+    mindeviationDPS = cinfo$ddeviationmin;
+    maxdeviationDPS = cinfo$ddeviationmax;
+    
+    cinfo = computeNormValue(plist$deltaStepsPerSecond);
+    mindeviationDSPS = cinfo$ddeviationmin;
+    maxdeviationDSPS = cinfo$ddeviationmax;
+    
+    cinfo = computeNormValue(plist$stepsPerSecond);
+    mindeviationSPS = cinfo$ddeviationmin;
+    maxdeviationSPS = cinfo$ddeviationmax;
+    
+    
+  }
+  
+  
+  list(medianDDPS=medianDDPS, medianDSPS= medianDSPS, medianDD= medianDD, medianDS= medianDS, medianDPS= medianDPS, medianSPS= medianSPS, iqrDDPS =iqrDDPS, iqrDSPS= iqrDSPS, iqrDD= iqrDD,iqrDS = iqrDS, iqrDPS= iqrDPS, iqrSPS= iqrSPS, mindeviationDPS = mindeviationDPS, maxdeviationDPS = maxdeviationDPS, mindeviationDDPS = mindeviationDDPS, maxdeviationDDPS = maxdeviationDDPS, mindeviationSPS =  mindeviationSPS, maxdeviationSPS = maxdeviationSPS, mindeviationDSPS = mindeviationDSPS, maxdeviationDSPS = maxdeviationDSPS);
+  
+  
+  
+}
+
+
+
+
+
 #' Pedometer Features
 #'
 #' @param rvObj 
